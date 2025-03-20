964d8540a1943571d379966198130b306e69f55d
Greg Benson <greg.benson@gmail.com>
2025-03-20 06:38:08 -0700

    Fixed redir.c, added redir2.c.

--- week09/xv6-riscv/user/redir2.c
+++ week09/xv6-riscv/user/redir2.c
@@ -13,27 +13,24 @@ main(int argc, char *argv[])
   argv_child[1] = NULL;
   int rv;
 
-  fd = open("out.txt", O_CREATE | O_WRONLY);
-  if(fd < 0){
-    printf("Cannot open(\"%s\")\n", "out.txt");
-    exit(-1);
-  }
-
   pid = fork();
 
   if(pid == 0){
     printf("In child\n");
 
     close(1); // close stdout
-    dup(fd); // duplicate fd2, put into slot 1
-    close(fd); // close off fd2
+
+    fd = open("out.txt", O_CREATE | O_WRONLY);
+    if(fd < 0){
+      printf("Cannot open(\"%s\")\n", "out.txt");
+      exit(-1);
+    }
 
     rv = exec(argv_child[0], argv_child);
     if(rv < 0)
       printf("Cannot exec(\"%s\")\n", argv_child[0]);
     exit(0);
   } else {
-    close(fd);
     printf("In parent\n");
     wait(0);
   }
